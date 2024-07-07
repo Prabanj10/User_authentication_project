1674857a5c331fe97c6ffe7304a51ccae13c8b51
Prabanj <prabanjk@gmail.com>
2024-07-08 03:45:07 +0530

    create user model

---
+++ server/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Your email is required'],
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: [true, 'Your username is required'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function () {
+  this.password = await bcrypt.hash(this.password, 8);
+});
+
+const User = mongoose.model('user', userSchema);
+
+export default User;
