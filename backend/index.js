0e4e246d72a020399a06c2928a3c2066a005b19c
Prabanj <prabanjk@gmail.com>
2024-07-08 02:54:26 +0530

    Connect to MongoDB and add CORS middleware

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,30 @@
 import express from 'express';
 import authrouter from './routes/oauthRoute.js';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 3000 
 
-app.use(express.urlencoded({extended:true}))
+const { MONGODB_URL, PORT } = process.env;
+const port = PORT || 3000;
 
-app.use('/',authrouter)
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => console.log('connected to database'))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use('/', authrouter);
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
 
 app.listen(port, (req, res) => {
-  console.log('server running on port 3000');
+  console.log(`server running on ${port}`);
 });
