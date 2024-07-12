d35ec3c6b72cd7a9bf649288bb27dff826b0124d
Prabanj <prabanjk@gmail.com>
2024-07-13 04:16:42 +0530

    fix: add missing return statement in function

--- server/utils/SecretToken.js
+++ server/utils/SecretToken.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const createSecretToken = (id) => {
-  jwt.sign({ id }, process.env.TOKEN_KEY, { expiresIn: 3 * 60 * 60 });
+  return jwt.sign({ id }, process.env.TOKEN_KEY, { expiresIn: 3 * 60 * 60 });
 };
 
 export default createSecretToken
