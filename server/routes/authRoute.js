ac3d5af18f95aa2300cc13d711fe3706b0cc8a5d
Prabanj <prabanjk@gmail.com>
2024-07-13 03:19:28 +0530

    add AuthMiddleware for user verification using JWT

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import { login, signup } from '../controllers/authControllers.js';
+import { userVerification } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login',login)
+router.post('/',userVerification)
 
 export default router;
