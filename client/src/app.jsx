38bd633a7fe5e70a9d7e8d3003eebb5a568e4ffb
Prabanj <prabanjk@gmail.com>
2024-07-17 03:58:44 +0530

    Added Pages and Routes

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,9 +1,27 @@
-
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
 
 const app = () => {
   return (
-    <div>app</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="signup"
+          element={<Signup />}
+        />
+        <Route
+          path="login"
+          element={<Login />}
+        />
+      </Routes>
+    </div>
+  );
+};
 
-export default app
+export default app;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,17 @@
-import { render } from 'preact'
-import  App  from './app.jsx'
-import './index.css'
+import { render } from 'preact';
+import App from './app.jsx';
+import './index.css';
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-render(<App />, document.getElementById('app'))
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app')
+);
